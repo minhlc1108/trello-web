2d3be9b7ca2cba80ac579d282cf283fcad9d52cf
minhlc1108 <minh.lcm.2003@gmail.com>
2024-01-28 14:43:13 +0700

    config mui theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import DeleteIcon from '@mui/icons-material/Delete'
+import Typography from '@mui/material/Typography'
 function App() {
 
   return (
     <>
       <div>minh</div>
+      <Typography variant='body2' color='text.secondary'>Test Typography</Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
