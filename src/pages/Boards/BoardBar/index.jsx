b24b1df06a3b6d39df7170de8c3ea886d17522b3
minhlc1108 <minh.lcm.2003@gmail.com>
2024-02-08 09:30:29 +0700

    Board Bar

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,15 +1,131 @@
 import Box from '@mui/system/Box'
+import Chip from '@mui/material/Chip'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import Tooltip from '@mui/material/Tooltip'
+import Button from '@mui/material/Button'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+
+const MENU_STYLES = {
+  color: 'primary.main',
+  backgroundColor: 'white',
+  border: 'none',
+  paddingX: '5px',
+  borderRadius: '4px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+}
 
 function BoardBar() {
   return (
     <Box sx={{
-      backgroundColor: 'primary.dark',
       width: '100%',
       height: (theme) => theme.trello.boardBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 2,
+      overflowX: 'auto',
+      borderTop: '1px solid #00bfa5',
+      paddingX: 2
     }}>
-      Board Bar
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          sx={MENU_STYLES}
+          icon={<DashboardIcon />}
+          label="MERN STACK Board"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<VpnLockIcon />}
+          label="Public/Private Workspaces"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<AddToDriveIcon />}
+          label="Add To Google Drive"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<BoltIcon />}
+          label="Automation"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<FilterListIcon />}
+          label="Filter"
+          clickable
+        />
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Button variant="outlined" startIcon={<PersonAddIcon />}>Invite</Button>
+        <AvatarGroup
+          max={7}
+          sx={{
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: 16
+            }
+          }}
+        >
+          <Tooltip title='minh'>
+            <Avatar alt="Minh Le" src="https://yt3.ggpht.com/yti/AGOGRCr6X5sBKPtPJZ5e9bPFEWkTDKX64WocLZ6zS1s6Vw=s88-c-k-c0x00ffffff-no-rj" />
+          </Tooltip>
+          <Tooltip title='minh'>
+            <Avatar alt="Minh Le" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-L6DYDB3WQ-ltV0OHiNXfM9FEAfUnhjgUaA&usqp=CAU" />
+          </Tooltip>
+          <Tooltip title='minh'>
+            <Avatar alt="Minh Le" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe_2cZMKysro3YpIWasiWcKy8bk8bHLelR0g&usqp=CAU" />
+          </Tooltip>
+          <Tooltip title='minh'>
+            <Avatar alt="Minh Le" src="https://png.pngtree.com/png-clipart/20211121/original/pngtree-funny-avatar-vector-icons-png-png-image_6948004.png" />
+          </Tooltip>
+          <Tooltip title='minh'>
+            <Avatar alt="Minh Le" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1qm4wFghbqBE1Yp_yt_ExCO5uYfqs_z2MtQ&usqp=CAU" />
+          </Tooltip>
+          <Tooltip title='minh'>
+            <Avatar alt="Minh Le" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx-dSBRaoSO3rE0KJ1lb9bjuJUE1eayGbJJA&usqp=CAU" />
+          </Tooltip>
+          <Tooltip title='minh'>
+            <Avatar alt="Minh Le" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSve1KURcIup7UuJVn0N1NbVv1bSDXLVKooXg&usqp=CAU" />
+          </Tooltip>
+          <Tooltip title='minh'>
+            <Avatar alt="Minh Le" src="https://yt3.ggpht.com/yti/AGOGRCr6X5sBKPtPJZ5e9bPFEWkTDKX64WocLZ6zS1s6Vw=s88-c-k-c0x00ffffff-no-rj" />
+          </Tooltip>
+          <Tooltip title='minh'>
+            <Avatar alt="Minh Le" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-L6DYDB3WQ-ltV0OHiNXfM9FEAfUnhjgUaA&usqp=CAU" />
+          </Tooltip>
+          <Tooltip title='minh'>
+            <Avatar alt="Minh Le" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe_2cZMKysro3YpIWasiWcKy8bk8bHLelR0g&usqp=CAU" />
+          </Tooltip>
+          <Tooltip title='minh'>
+            <Avatar alt="Minh Le" src="https://png.pngtree.com/png-clipart/20211121/original/pngtree-funny-avatar-vector-icons-png-png-image_6948004.png" />
+          </Tooltip>
+          <Tooltip title='minh'>
+            <Avatar alt="Minh Le" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1qm4wFghbqBE1Yp_yt_ExCO5uYfqs_z2MtQ&usqp=CAU" />
+          </Tooltip>
+          <Tooltip title='minh'>
+            <Avatar alt="Minh Le" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx-dSBRaoSO3rE0KJ1lb9bjuJUE1eayGbJJA&usqp=CAU" />
+          </Tooltip>
+          <Tooltip title='minh'>
+            <Avatar alt="Minh Le" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSve1KURcIup7UuJVn0N1NbVv1bSDXLVKooXg&usqp=CAU" />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
