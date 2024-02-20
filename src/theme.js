cbfbd6e83eea6e1c4c661169cdb22e4e74a12002
minhlc1108 <minh.lcm.2003@gmail.com>
2024-02-20 22:28:30 +0700

    board content

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,14 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
-import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 // Create a theme instance.
+const APP_BAR_HEIGHT = '58px'
+const BOARD_BAR_HEIGHT = '60px'
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
 
 const theme = extendTheme({
   trello: {
-    appBarHeight: '58px',
-    boardBarHeight: '60px'
+    appBarHeight: APP_BAR_HEIGHT,
+    boardBarHeight: BOARD_BAR_HEIGHT,
+    boardContentHeight: BOARD_CONTENT_HEIGHT
   },
   colorSchemes: {
     // light: {
@@ -55,14 +58,27 @@ const theme = extendTheme({
         root: {
           fontSize: '0.875rem'
         }
-      },
-      MuiOutlinedInput: {
-        styleOverrides: {
-          root: {
-            fontSize: '0.875rem',
-            '& fieldset': { borderWidth: '0.5px !important' },
-            '&:hover fieldset': { borderWidth: '1px !important' },
-            '&.Mui-focused fieldset': { borderWidth: '1px !important' }
+      }
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          '&.MuiTypography-body1': { fontSize: '0.875rem' }
+        }
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          fontSize: '0.875rem',
+          '& fieldset': {
+            borderWidth: '1x !important'
+          },
+          '&:hover fieldset': {
+            borderWidth: '2px !important'
+          },
+          '&.Mui-focused fieldset': {
+            borderWidth: '2px !important'
           }
         }
       }
