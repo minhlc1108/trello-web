593011a0478992a2133b96867ccdaac2fc44da7c
minhlc1108 <minh.lcm.2003@gmail.com>
2024-02-21 23:23:22 +0700

    refactor struct Board - Columns - Cards

---
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -0,0 +1,33 @@
+import Box from '@mui/system/Box'
+import Card from './Card/Card'
+
+function ListCard() {
+  return (
+    < Box sx={{
+      margin: '0 5px',
+      padding: '0 5px',
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 1,
+      overflowX: 'hidden',
+      overflowY: 'auto',
+      maxHeight: (theme) => `calc(
+        ${theme.trello.boardContentHeight} - 
+        ${theme.spacing(5)} - 
+        ${theme.trello.columnHeaderHeight} - 
+        ${theme.trello.columnFooterHeight}
+        )`,
+      '&::-webkit-scrollbar-thumb': { backgroundColor: '#ced0da' },
+      '&::-webkit-scrollbar-thumb:hover': { backgroundColor: '#bfc2cf' }
+    }}>
+      <Card />
+      <Card temporatyHidenMedia />
+      <Card temporatyHidenMedia />
+      <Card temporatyHidenMedia />
+      <Card temporatyHidenMedia />
+      <Card temporatyHidenMedia />
+    </Box >
+  )
+}
+
+export default ListCard
