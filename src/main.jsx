425b1f1c8eb86cf4f732ffe6cac039687cf81279
minhlc1108 <minh.lcm.2003@gmail.com>
2024-03-26 23:32:38 +0700

    custom collision detection strategy

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import CssBaseline from '@mui/material/CssBaseline'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from '~/theme'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CssVarsProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </CssVarsProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <CssVarsProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </CssVarsProvider>
+  // </React.StrictMode>
 )
