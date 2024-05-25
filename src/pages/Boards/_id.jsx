de666b4e83a21a30f166150c492720723e82e2a9
minhlc1108 <minh.lcm.2003@gmail.com>
2024-05-25 22:34:46 +0700

    axios call api get board

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,14 +1,26 @@
+import { useEffect, useState } from 'react'
 import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { mockData } from '~/apis/mock-data'
+import { fetchBoardDetailsAPI } from '~/apis'
 function Board() {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    // tạm thời fix cứng boardId, sau dùng react-router-dom để lấy boardId từ Url về
+    const boardId = '664eca800134880a8b2d8415'
+    // Call API
+    fetchBoardDetailsAPI(boardId).then((board) => {
+      setBoard(board)
+    })
+  }, [])
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container >
   )
 }
